37ab1658c2ac81ffb5b3e2c79e47921f5a217e45
ruomuc <252615299@qq.com>
2019-01-11 21:25:38 +0800

    update

--- async await的异步/test.js
+++ async await的异步/test.js
@@ -3,26 +3,63 @@ function a(tag) {
         if (tag == 1) {
             resolve('normal return');
         } else {
-            reject(new Error("requires promises as the param"));
+            reject(new Error("a"));
+        }
+    })
+}
+
+function aa(tag) {
+    return new Promise((resolve, reject) => {
+        if (tag == 1) {
+            resolve('normal return');
+        } else {
+            reject(new Error("aa"));
+        }
+    })
+}
+
+function aaa(tag) {
+    return new Promise((resolve, reject) => {
+        if (tag == 1) {
+            resolve('normal return');
+        } else {
+            reject(new Error("aaa"));
         }
     })
 }
 
 async function b() {
+
     //--------------------1
     try {
-        let temp = await a(0);
+        let temp = await a(1);
         console.log('temp:', temp);
-        return new Promise((resolve, reject) => {
-            if (temp) {
-                resolve(temp);
-            } else {
-                resolve(null);
-            }
-        })
+        if (temp) {
+            let temp2 = await aa(0);
+            console.log('xxxxxxxxxxxx');
+            return 'aaaa';
+        } else {
+            resolve(null);
+        }
     } catch (e) {
         console.log('temp:', e);
     }
+    // --------------------
+
+    //--------------------1
+    // try {
+    //     let temp = await a(0);
+    //     console.log('temp:', temp);
+    //     return new Promise((resolve, reject) => {
+    //         if (temp) {
+    //             resolve(temp);
+    //         } else {
+    //             resolve(null);
+    //         }
+    //     })
+    // } catch (e) {
+    //     console.log('temp:', e);
+    // }
     // --------------------
 
 
--- 读取excel表格/excel.js
+++ 读取excel表格/excel.js
@@ -8,51 +8,53 @@ var savePath = ['./client/client-document.js', './server/server-document.js'];
 
 console.log('__dirname', __dirname);
 
-const workSheetsFromFile = xlsx.parse(`${__dirname}/Events.xls`);
+const workSheetsFromFile = xlsx.parse(`${__dirname}/111222211111.xlsx`);
 
-const data_json = [];
+fs.writeFileSync(savePath[0], JSON.stringify(workSheetsFromFile), { 'flag': 'w' });
 
-var len = workSheetsFromFile.length;
-// for (let i = 0; i < len; ++i) {
-var wsfi = workSheetsFromFile[0]; //暂时只有sheet有数据，否则定义data_json1 和 data_json2 。
-var sheet_name = wsfi.name;
-console.log('sheet_name', sheet_name);
+// const data_json = [];
 
-if (wsfi.data[0] != null) {
-    //先循环标题，键
-    var len2 = wsfi.data;
+// var len = workSheetsFromFile.length;
+// // for (let i = 0; i < len; ++i) {
+// var wsfi = workSheetsFromFile[0]; //暂时只有sheet有数据，否则定义data_json1 和 data_json2 。
+// var sheet_name = wsfi.name;
+// console.log('sheet_name', sheet_name);
 
-    var len4 = wsfi.data.length;
-    console.log('len4', len4);
-    for (let k = 0; k < len4; ++k) {
-        if (k != 0) {//第一个数组已经被当做键了
-            var obj = {};
-            var wsfk = wsfi.data[k];
-            var len5 = wsfk.length;
-            console.log('len5', len5);
-            for (let m = 0; m < len5; ++m) {
-                var wsfm = wsfk[m];
-                var wsfn = wsfi.data[0][m];
+// if (wsfi.data[0] != null) {
+//     //先循环标题，键
+//     var len2 = wsfi.data;
 
-                obj[wsfn] = wsfm;
-                if (m == len5 - 1) {
-                    data_json.push(obj);
-                }
-            }
-        }
-    }
-}
-console.log('data_json', data_json, data_json.length);
+//     var len4 = wsfi.data.length;
+//     console.log('len4', len4);
+//     for (let k = 0; k < len4; ++k) {
+//         if (k != 0) {//第一个数组已经被当做键了
+//             var obj = {};
+//             var wsfk = wsfi.data[k];
+//             var len5 = wsfk.length;
+//             console.log('len5', len5);
+//             for (let m = 0; m < len5; ++m) {
+//                 var wsfm = wsfk[m];
+//                 var wsfn = wsfi.data[0][m];
 
-for (let i = 0; i < savePath.length; ++i) {
-    var path = savePath[i];
-    if (i == 0) {
-        var data = JSON.stringify(data_json);
-        console.log('data1', data, path);
-        fs.writeFileSync(path, data, { 'flag': 'w' });
-    } else {
-        var data = "module.exports.planConfig = " + JSON.stringify(data_json) + "";
-        fs.writeFileSync(path, data, { 'flag': 'w' });
-    }
-}
+//                 obj[wsfn] = wsfm;
+//                 if (m == len5 - 1) {
+//                     data_json.push(obj);
+//                 }
+//             }
+//         }
+//     }
 // }
+// console.log('data_json', data_json, data_json.length);
+
+// for (let i = 0; i < savePath.length; ++i) {
+//     var path = savePath[i];
+//     if (i == 0) {
+//         var data = JSON.stringify(data_json);
+//         console.log('data1', data, path);
+//         fs.writeFileSync(path, data, { 'flag': 'w' });
+//     } else {
+//         var data = "module.exports.planConfig = " + JSON.stringify(data_json) + "";
+//         fs.writeFileSync(path, data, { 'flag': 'w' });
+//     }
+// }
+// // }
