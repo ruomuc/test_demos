b45fccb695b4834a01b94ab9406149aa9b6981c7
ruomu <zm252615299@gmail.com>
2020-08-16 23:17:16 +0800

    ✨ 特性: orm-test

--- orm-test/sequelize.js
+++ orm-test/sequelize.js
@@ -1,1 +1,24 @@
 const Sequelize = require('sequelize');
+
+const sequelize = new Sequelize('test', 'root', 'rootroot', {
+  host: 'localhost',
+  dialect: 'mysql',
+  port: '3306',
+  operatorsAliases: false,
+  timezone: '+08:00', // 时区，中国+8
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  }
+});
+
+sequelize
+    .authenticate()
+    .then(() => {
+      console.log('***数据库连接成功');
+    })
+    .catch(err => {
+      console.error('数据库连接失败：', err);
+    });
