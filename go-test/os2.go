a23a7e6bb889c40828fb370553eb03aa4abf3bf8
ruomu <252615299@qq.com>
2020-08-16 19:02:41 +0800

    go-test

--- go-test/os2.go
+++ go-test/os2.go
@@ -2,10 +2,7 @@ package main
 
 import (
 	"bufio"
-	"fmt"
-	"io/ioutil"
 	"os"
-	"strings"
 )
 
 // func main() {
@@ -41,22 +38,22 @@ func countLines(f *os.File, counts map[string]int) {
 	}
 }
 
-func main() {
-	counts := make(map[string]int)
-	for _, filename := range os.Args[1:] {
-		data, err := ioutil.ReadFile(filename)
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "dup3: %v\n", err)
-			continue
-		}
-		fmt.Println("file_data=", data, string(data))
-		for _, line := range strings.Split(string(data), "\n") {
-			counts[line]++
-		}
-	}
-	for line, n := range counts {
-		if n > 1 {
-			fmt.Printf("%d\t%s\n", n, line)
-		}
-	}
-}
+// func main() {
+// 	counts := make(map[string]int)
+// 	for _, filename := range os.Args[1:] {
+// 		data, err := ioutil.ReadFile(filename)
+// 		if err != nil {
+// 			fmt.Fprintf(os.Stderr, "dup3: %v\n", err)
+// 			continue
+// 		}
+// 		fmt.Println("file_data=", data, string(data))
+// 		for _, line := range strings.Split(string(data), "\n") {
+// 			counts[line]++
+// 		}
+// 	}
+// 	for line, n := range counts {
+// 		if n > 1 {
+// 			fmt.Printf("%d\t%s\n", n, line)
+// 		}
+// 	}
+// }
