d2628c7894f598a5d5633105bb272d2ef1899968
ruomu <252615299@qq.com>
2020-08-26 23:20:24 +0800

    ✨ 特性: ✨ 特性: ✨ 特性: go-test

--- go-test/goroutine.go
+++ go-test/goroutine.go
@@ -1,50 +1,40 @@
 package main
 
-import (
-	"bufio"
-	"fmt"
-	"io"
-	"net/http"
-	"os"
-	"strings"
-	"time"
-)
+// func main() {
+// 	start := time.Now()
+// 	ch := make(chan string)
+// 	for _, url := range os.Args[1:] {
+// 		ok := strings.HasPrefix(url, "http://")
+// 		if ok == false {
+// 			url = "http://" + url
+// 		}
+// 		go fetch(url, ch)
+// 	}
 
-func main() {
-	start := time.Now()
-	ch := make(chan string)
-	for _, url := range os.Args[1:] {
-		ok := strings.HasPrefix(url, "http://")
-		if ok == false {
-			url = "http://" + url
-		}
-		go fetch(url, ch)
-	}
+// 	for range os.Args[1:] {
+// 		fmt.Println(<-ch)
+// 	}
+// 	fmt.Printf("%.2fs elapsed\n", time.Since(start).Seconds())
+// }
 
-	for range os.Args[1:] {
-		fmt.Println(<-ch)
-	}
-	fmt.Printf("%.2fs elapsed\n", time.Since(start).Seconds())
-}
+// func fetch(url string, ch chan<- string) {
+// 	start := time.Now()
+// 	resp, err := http.Get(url)
+// 	if err != nil {
+// 		ch <- fmt.Sprint(err)
+// 		return
+// 	}
+// 	out, err := os.Create("./resp.html")
+// 	wt := bufio.NewWriter(out)
+// 	nbytes, err := io.Copy(wt, resp.Body)
+// 	resp.Body.Close()
 
-func fetch(url string, ch chan<- string) {
-	start := time.Now()
-	resp, err := http.Get(url)
-	if err != nil {
-		ch <- fmt.Sprint(err)
-		return
-	}
-	out, err := os.Create("./resp.txt")
-	wt := bufio.NewWriter(out)
-	nbytes, err := io.Copy(wt, resp.Body)
-	resp.Body.Close()
+// 	if err != nil {
+// 		ch <- fmt.Sprintf("while reading %s: %v\n", url, err)
+// 		return
+// 	}
 
-	if err != nil {
-		ch <- fmt.Sprintf("while reading %s: %v\n", url, err)
-		return
-	}
+// 	secs := time.Since(start).Seconds()
+// 	ch <- fmt.Sprintf("%.2fs  %7d %s", secs, nbytes, url)
 
-	secs := time.Since(start).Seconds()
-	ch <- fmt.Sprintf("%.2fs  %7d %s", secs, nbytes, url)
-
-}
+// }
