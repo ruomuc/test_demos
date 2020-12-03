96742e2604cccb563c858f9c5670acbaeec6063a
ruomu <zm252615299@gmail.com>
2020-12-03 18:59:23 +0800

    ✨ 特性: go-test

---
+++ go-test/gin/assets/app.js
@@ -0,0 +1,1 @@
+console.log("push success app.js11")
--- go-test/gin/main.go
+++ go-test/gin/main.go
@@ -1,10 +1,17 @@
 package main
 
 import (
+	"log"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
+
+// LoginForm ...
+type LoginForm struct {
+	Password string `form:"password" binding:"required"`
+	User     string `form:"user" binding:"required"`
+}
 
 // func main() {
 // 	r := gin.Default()
@@ -22,6 +29,9 @@ func main() {
 // SetupRouter 设置路由
 func SetupRouter() *gin.Engine {
 	r := gin.Default()
+	r.Delims("[[[", "]]]")
+	r.LoadHTMLGlob("templates/*")
+	r.Static("assets", "./assets/")
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{"message": "pong"})
 	})
@@ -31,6 +41,61 @@ func SetupRouter() *gin.Engine {
 			"tag":  "<br>",
 		}
 		c.AsciiJSON(http.StatusOK, data)
+	})
+	r.GET("/index", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.html", gin.H{
+			"title": "Main website",
+		})
+	})
+	// 没有https的证书，暂无法验证。。
+	r.GET("/push", func(c *gin.Context) {
+		if pusher := c.Writer.Pusher(); pusher != nil {
+			if err := pusher.Push("/assets/app.js", nil); err != nil {
+				log.Printf("Falied to push : %v", err)
+			}
+		}
+		c.HTML(200, "push.html", gin.H{
+			"status": "success",
+		})
+	})
+	r.GET("/JSONP?callback=x", func(c *gin.Context) {
+		data := map[string]interface{}{
+			"foo": c.Query("callback"),
+		}
+		c.JSONP(http.StatusOK, data)
+	})
+
+	r.POST("/login", func(c *gin.Context) {
+		var form LoginForm
+		if c.ShouldBind(&form) == nil {
+			if form.User == "user" && form.Password == "pwd" {
+				c.JSON(200, gin.H{"status": "you are logged in"})
+			} else {
+				c.JSON(401, gin.H{"status": "unauthorized"})
+			}
+		}
+	})
+
+	r.POST("/form-post", func(c *gin.Context) {
+		message := c.PostForm("message")
+		nick := c.DefaultPostForm("nick", "anonymous")
+		c.JSON(200, gin.H{
+			"status":  "posted",
+			"message": message,
+			"nick":    nick,
+		})
+	})
+
+	r.GET("/json", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"html": "<b>hello world!</b>",
+		})
+	})
+
+	r.GET("/purejson", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"html": "<b>hello world!</b>",
+		})
 	})
 	return r
 }
