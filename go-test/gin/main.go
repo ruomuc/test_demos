b19141c0eacd290c73e63378d4a4e22421710504
ruomu <zm252615299@gmail.com>
2020-12-05 13:51:14 +0800

    ✨ 特性: go-test  gin

--- go-test/gin/main.go
+++ go-test/gin/main.go
@@ -97,5 +97,51 @@ func SetupRouter() *gin.Engine {
 			"html": "<b>hello world!</b>",
 		})
 	})
+
+	r.GET("/secureJSON", func(c *gin.Context) {
+		names := []string{"a", "b", "c"}
+		c.SecureJSON(200, names)
+	})
+
+	r.GET("/someJSON", func(c *gin.Context) {
+		c.JSON(200, gin.H{"message": "hello", "status": http.StatusOK})
+	})
+
+	r.GET("moreJSON", func(c *gin.Context) {
+		var msg struct {
+			Name    string `json:"user"`
+			Message string
+			Number  int
+		}
+		msg.Name = "zm"
+		msg.Message = "hello"
+		msg.Number = 21
+		c.JSON(http.StatusOK, msg)
+	})
+
+	r.GET("/someXML", func(c *gin.Context) {
+		c.XML(http.StatusOK, gin.H{"message": "hey", "status": http.StatusBadRequest})
+	})
+
+	r.GET("/someYAML", func(c *gin.Context) {
+		c.YAML(http.StatusOK, gin.H{"message": "hey", "status": http.StatusBadRequest})
+	})
+
+	r.GET("/someDataFromReader", func(c *gin.Context) {
+		response, err := http.Get("https://s0.lgstatic.com/i/image/M00/49/93/Ciqc1F9PYuuAQINxAAA236heaL0459.png")
+		if err != nil || response.StatusCode != http.StatusOK {
+			c.Status(http.StatusServiceUnavailable)
+			return
+		}
+		reader := response.Body
+		contentLength := response.ContentLength
+		contentType := response.Header.Get("Content-Type")
+
+		extraHeaders := map[string]string{
+			"Content-Disposition": `attachment;filename="gopher.png"`,
+		}
+		c.DataFromReader(http.StatusOK, contentLength, contentType, reader, extraHeaders)
+	})
+
 	return r
 }
