e585bc48707f3aa24dafe460f31b90b98e5e5540
ruomu <zm252615299@gmail.com>
2021-01-04 14:17:38 +0800

    ✨ 特性: 删掉一些没用的

--- hiKoa-master/routes/about.js
+++
@@ -1,9 +0,0 @@
-// routes/about.js
-module.exports = {
-    async index(ctx, next) {
-        await ctx.render('about', {
-            title: '首页',
-            type:'home',
-        })
-    }
-}
--- hiKoa-master/routes/home.js
+++
@@ -1,9 +0,0 @@
-// routes/home.js
-module.exports = {
-    async index(ctx, next) {
-        await ctx.render('index', {
-            title: '首页',
-            type:'home',
-        })
-    }
-}
